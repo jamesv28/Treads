a6d1043cc5e515e71e420aaa3273f6a4160311b1
James Volmert <james.volmert@gmail.com>
2019-07-25 16:12:03 -0600

    connected outlets to start getting functionality

---
+++ Treads/Controller/CurrentRunVC.swift
@@ -0,0 +1,30 @@
+//
+//  CurrentRunVC.swift
+//  Treads
+//
+//  Created by James Volmert on 7/25/19.
+//  Copyright © 2019 James Volmert. All rights reserved.
+//
+
+import UIKit
+
+class CurrentRunVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
--- Treads/Controller/StartRunVC.swift
+++ Treads/Controller/StartRunVC.swift
@@ -7,14 +7,38 @@
 //
 
 import UIKit
+import MapKit
 
-class StartRunVC: UIViewController {
+class StartRunVC: LocationVC {
 
+    @IBOutlet weak var mapView: MKMapView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        checkLocationAuthStatus()
+        mapView.delegate = self
     }
-
-
+    
+    override func viewWillAppear(_ animated: Bool) {
+        manager?.delegate = self
+        manager?.startUpdatingLocation()
+    }
+    
+    override func viewDidDisappear(_ animated: Bool) {
+        manager?.stopUpdatingLocation()
+    }
+    
+    @IBAction func locationCenterBtnPressed(_ sender: Any) {
+    }
+    
 }
 
+extension StartRunVC: CLLocationManagerDelegate {
+    func locationManager(_ manager: CLLocationManager, didChangeAuthorization status: CLAuthorizationStatus) {
+        if status == .authorizedWhenInUse {
+            checkLocationAuthStatus()
+            mapView.showsUserLocation = true
+            mapView.userTrackingMode = .follow
+        }
+    }
+}
